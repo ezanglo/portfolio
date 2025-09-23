12259605f50fbc1971b5263cced34d2be217df5e
Ezra Anglo <dev.ezraanglo@gmail.com>
2025-09-24 00:50:46 +0800

    Implemented PayloadCMS

---
+++ collections/Users.ts
@@ -0,0 +1,22 @@
+import { CollectionConfig } from "payload";
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  auth: true,
+  admin: {
+    useAsTitle: 'email',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'email',
+      type: 'email',
+      required: true,
+      unique: true,
+    },
+  ],
+}
