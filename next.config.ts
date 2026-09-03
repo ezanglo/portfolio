3301cb734e1127d66a7a6893bb69f2db9cc93375
Ezra Anglo <dev.ezraanglo@gmail.com>
2026-09-03 14:50:48 +0800

    feat: add image quality settings to next.config for improved image handling

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['sharp'],
+  images: {
+    qualities: [75, 95],
+  },
 };
 
 export default withPayload(nextConfig) 
